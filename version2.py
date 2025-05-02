bbb14f7328ba84cd91ec21b4ba646f2dc09bd21b
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-05-02 15:48:53 +0100

    init2

---
+++ version2.py
@@ -0,0 +1,9 @@
+import random
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('900x600')
+root.title('Encryption Decryption Progam v.2.0')
+root.config(background='#01050d')
+
+root.mainloop()
